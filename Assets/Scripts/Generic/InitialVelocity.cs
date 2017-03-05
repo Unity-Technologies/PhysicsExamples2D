20748d782bacbca5ea0f826da3f938ddbcd98b1c
Melv May <melv.may@gmail.com>
2017-03-05 21:37:58 +0000

    Added PhysicsMaterial2D scene.

---
+++ Assets/Scripts/Generic/InitialVelocity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Set the initial velocity of a Rigidbody2D.
+/// </summary>
+public class InitialVelocity : MonoBehaviour
+{
+    public Vector2 m_Velocity;
+    public float m_AngularVelocity;
+
+	void Start ()
+    {
+        var rigidbody = GetComponent<Rigidbody2D> ();
+
+        // Set the initial velocities if the rigidbody was found.
+        if (rigidbody)
+        {
+            rigidbody.velocity = m_Velocity;
+            rigidbody.angularDrag = m_AngularVelocity;
+        }
+	}
+}
