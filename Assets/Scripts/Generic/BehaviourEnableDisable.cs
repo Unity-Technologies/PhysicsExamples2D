e7438d62f4a8d3ac0a4798e46f70c0cda5b21404
Melv May <melv.may@gmail.com>
2017-03-05 18:44:13 +0000

    Added SliderJoint2D scenes.

---
+++ Assets/Scripts/Generic/BehaviourEnableDisable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Enable/Disable a component periodically.
+/// </summary>
+public class BehaviourEnableDisable : MonoBehaviour
+{
+    public Behaviour m_BehaviourComponent;
+    public float m_TogglePeriod = 1f;
+
+	IEnumerator Start ()
+	{
+        if (m_BehaviourComponent)
+        {
+            while (true)
+            {
+                // Flip the enabled state.
+                m_BehaviourComponent.enabled = !m_BehaviourComponent.enabled;
+
+                // Wait for specified period before toggling again.
+                yield return new WaitForSeconds (m_TogglePeriod);
+            }
+        }		
+	}
+	
+}
