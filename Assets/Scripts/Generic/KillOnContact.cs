b92b2f18e43599696f0aec8de76aae44bd53ae05
Melv May <melv.may@gmail.com>
2017-03-05 16:00:42 +0000

    Added FrictionJoint2D scenes.

---
+++ Assets/Scripts/Generic/KillOnContact.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Kill any object coming into contact with this object.
+/// </summary>
+public class KillOnContact : MonoBehaviour
+{
+    public bool m_KillCollision = true;
+    public bool m_KillTrigger = true;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (m_KillCollision)
+            Destroy (collision.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (m_KillTrigger)
+            Destroy (collision.gameObject);
+    }
+}
