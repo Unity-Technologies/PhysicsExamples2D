38e8ce83aaed1850422086df7e932023cf181e5b
Melv May <melv.may@gmail.com>
2017-03-05 15:16:00 +0000

    Added RelativeJoint2D scenes.

---
+++ Assets/Scripts/SceneSpecific/RelativeJoint2D_AnimateAngularOffset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Animate the angular offset of any RelativeJoint2D on the current GameObject.
+/// </summary>
+public class RelativeJoint2D_AnimateAngularOffset : MonoBehaviour
+{
+    public float m_RotationSpeed = 90.0f;
+
+    private RelativeJoint2D relativeJoint;
+
+	void Start ()
+	{
+        relativeJoint = GetComponent<RelativeJoint2D> ();	
+	}
+	
+	void FixedUpdate ()
+	{
+        // Finish if we have no joint.
+        if (relativeJoint == null)
+            return;
+
+        // Animate the angular offset.
+        relativeJoint.angularOffset = relativeJoint.angularOffset + (m_RotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SceneSpecific/RelativeJoint2D_AnimateLinearOffset.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Animate the linear offset of any RelativeJoint2D on the current GameObject.
+/// </summary>
+public class RelativeJoint2D_AnimateLinearOffset : MonoBehaviour
+{
+    public float m_AnimateSpeed = 1f;
+    public float m_LinearAmplitude = 2f;
+    public float m_Direction = 0.0f;
+
+    private RelativeJoint2D relativeJoint;
+    private float time = 0f;
+
+	void Start ()
+	{
+        relativeJoint = GetComponent<RelativeJoint2D> ();	
+	}
+	
+	void FixedUpdate ()
+	{
+        // Finish if we have no joint.
+        if (relativeJoint == null)
+            return;
+
+        // Calculate linear offset.
+        var direction = m_Direction * Mathf.Deg2Rad;
+        var amplitude = Mathf.PingPong (Mathf.Sin (time) * m_LinearAmplitude, m_LinearAmplitude);
+
+        // Animate the angular offset.
+        relativeJoint.linearOffset = new Vector2 (Mathf.Cos (direction) * amplitude, Mathf.Sin (direction) * amplitude);
+
+        // Increase time.
+        time += (m_AnimateSpeed / m_LinearAmplitude) * Time.deltaTime;
+    }
+
+}
