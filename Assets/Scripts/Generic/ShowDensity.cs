e6b467705f8bd900a63b03867519ff476719325e
Melv May <melv.may@gmail.com>
2017-04-03 19:31:25 +0100

    Add BuoyancyEffector2D scene.

---
+++ Assets/Scripts/Generic/ShowDensity.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Show Density of a Collider2D.
+/// </summary>
+public class ShowDensity : MonoBehaviour
+{
+    public Collider2D m_Collider;
+	public TextMesh m_Text;
+
+	void Start ()
+	{
+		if (!m_Collider)
+			m_Collider = GetComponentInParent<Collider2D> ();
+
+		if (!m_Text)
+			m_Text = GetComponent<TextMesh> ();
+	}
+	
+	void FixedUpdate()
+	{
+		if (!m_Text || !m_Collider)
+			return;		
+
+		// Set the text to show current values.
+		m_Text.text = "Density = " + m_Collider.density.ToString("n2");
+	} 
+}
