de5078c74f05682599ee0f1d286766612ac6d2c8
Melvyn May <melv@unity3d.com>
2019-08-01 12:51:57 +0100

    Add Quadratic drag example.
    A good example of this can be found here: https://www.gdcvault.com/play/1021921/Designing-with-Physics-Bend-the

---
+++ Assets/Scripts/Generic/AddJumpForce.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class AddJumpForce : MonoBehaviour
+{
+    public KeyCode JumpKey = KeyCode.Space;
+    public float JumpImpulse = 9f;
+    public LayerMask GroundingLayer;
+
+    private Rigidbody2D Body;
+    private bool DoJump;
+
+    void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Can only jump if touching grounding layer(s) and the jump key has been pressed.
+        if (Body.IsTouchingLayers(GroundingLayer.value) &&
+            Input.GetKeyDown(JumpKey))
+        {
+            DoJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Add jump impulse if required.
+        if (DoJump)
+        {
+            Body.AddForce(new Vector2(0f, JumpImpulse), ForceMode2D.Impulse);
+
+            DoJump = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SceneSpecific/Miscellaneous_QuadraticDrag.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Miscellaneous_QuadraticDrag : MonoBehaviour
+{
+    public float DragCoefficient = 0.5f;
+
+    private Rigidbody2D Body;
+
+    void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // Fetch the body velocity.
+        var velocity = Body.velocity;
+
+        // Calculate the body speed.
+        var speed = velocity.magnitude;
+        if (speed > 0f)
+        {
+            // Calculate and apply the quadratic drag force (speed squared).
+            var dragForce = (DragCoefficient * speed * speed) * -velocity.normalized;
+            Body.AddForce(dragForce, ForceMode2D.Force);
+        }
+    }
+}
