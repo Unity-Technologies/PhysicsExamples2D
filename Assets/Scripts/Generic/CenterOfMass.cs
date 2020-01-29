cb19287c4af02364d4c8c455f60ab45e427426e3
Melvyn May <melv@unity3d.com>
2020-01-29 16:27:10 +0000

    Add Rigidbody2D Center-of-mass scene.

---
+++ Assets/Scripts/Generic/CenterOfMass.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterOfMass : MonoBehaviour
+{
+    public Vector2 LocalCenterOfMass = Vector2.zero;
+
+    private Rigidbody2D m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        UpdateCenterOfMass();
+    }
+
+    void OnValidate()
+    {
+        UpdateCenterOfMass();       
+    }
+
+    void OnDrawGizmos()
+    {
+        if (m_Rigidbody)
+            Gizmos.DrawWireSphere(m_Rigidbody.worldCenterOfMass, 0.1f);
+    }
+
+    void UpdateCenterOfMass()
+    {
+        if (m_Rigidbody)
+            m_Rigidbody.centerOfMass = LocalCenterOfMass;
+    }
+}
