82a470e0bd7a7a4ee93265e93d6e54914c56336c
Melvyn May <melv@unity3d.com>
2020-02-05 15:26:06 +0000

    Add DynamicGravity to IsGrounded scene.

---
+++ Assets/Scripts/Generic/DynamicGravity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class DynamicGravity : MonoBehaviour
+{
+    public float AgainstGravityScale = 1f;
+    public float FallGravityScale = 2f;
+
+    private Rigidbody2D m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // Calculate our direction relative to the global gravity.
+        var direction = Vector2.Dot(m_Rigidbody.velocity, Physics2D.gravity);
+        
+        // Set the gravity scale accordingly.
+        m_Rigidbody.gravityScale = direction > 0f ? FallGravityScale : AgainstGravityScale;
+    }
+}
