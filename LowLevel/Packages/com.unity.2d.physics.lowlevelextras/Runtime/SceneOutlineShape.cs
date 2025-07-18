907ab8f300fa29e23650d94ce546fff175a23b2b
Melvyn May <melv@unity3d.com>
2025-07-18 20:31:56 +0100

    Change CallbackTarget to be MonoBehaviour.

--- LowLevel/Packages/com.unity.2d.physics.lowlevelextras/Runtime/SceneOutlineShape.cs
+++ LowLevel/Packages/com.unity.2d.physics.lowlevelextras/Runtime/SceneOutlineShape.cs
@@ -13,7 +13,7 @@ namespace UnityEngine.U2D.Physics.LowLevelExtras
         public Vector2[] Points = { Vector2.left + Vector2.down, Vector2.right + Vector2.down, Vector2.right + Vector2.up, Vector2.left + Vector2.up };
         public PhysicsShapeDefinition ShapeDefinition = PhysicsShapeDefinition.defaultDefinition;
         public PhysicsUserData UserData;
-        public Object CallbackTarget;
+        public MonoBehaviour CallbackTarget;
         public SceneBody SceneBody;
 
         private struct OwnedShapes
