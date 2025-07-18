859873268cf47f632251bbbc3f2ae0a50eff41e5
Melvyn May <melv@unity3d.com>
2025-07-18 13:29:38 +0100

    Minor updates.

--- LowLevel/Sandbox/Assets/Editor/PhysicsDevUtility.cs
+++ LowLevel/Sandbox/Assets/Editor/PhysicsDevUtility.cs
@@ -1,19 +1,19 @@
-using System;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public static class PhysicsDevUtility
 {
     [MenuItem("Physics-Dev/Resave All Scenes")]
     public static void ResaveScenes()
     {
-        String[] allFiles = Directory.GetFiles(
+        var allFiles = Directory.GetFiles(
             Application.dataPath, "*.unity", SearchOption.AllDirectories
         );
 
-        for (int i=0; i<allFiles.Length; ++i)
+        for (var i=0; i < allFiles.Length; ++i)
         {
             allFiles[i] = "Assets" + allFiles[i].Replace(Application.dataPath, "");
         }
@@ -21,7 +21,7 @@ public static class PhysicsDevUtility
         foreach(var file in allFiles)
         {
             EditorSceneManager.OpenScene(file);
-            EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), file);
+            EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), file);
         }
     }
 }
