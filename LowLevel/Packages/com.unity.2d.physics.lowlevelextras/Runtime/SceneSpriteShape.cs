2fb7bab8844a9b702189873db198ff202ceeca9f
Melvyn May <melv@unity3d.com>
2025-07-20 15:06:15 +0100

    SceneSpriteShape (WIP)

---
+++ LowLevel/Packages/com.unity.2d.physics.lowlevelextras/Runtime/SceneSpriteShape.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine.LowLevelPhysics2D;
+
+namespace UnityEngine.U2D.Physics.LowLevelExtras
+{
+    [ExecuteAlways]
+    [DefaultExecutionOrder(PhysicsLowLevelExtrasExecutionOrder.SceneShape)]
+    [AddComponentMenu("Physics 2D (LowLevel)/Scene Sprite Shape", 1)]
+    [Icon(IconUtility.IconPath + "SceneShape.png")]
+    public sealed class SceneSpriteShape : MonoBehaviour
+    {
+        public Sprite Sprite;
+        public PhysicsShapeDefinition ShapeDefinition = PhysicsShapeDefinition.defaultDefinition;
+        public PhysicsUserData UserData;
+        public MonoBehaviour CallbackTarget;
+        public SceneBody SceneBody;
+
+        private List<Vector2> m_PhysicsShapeVertex = new();
+
+        private struct OwnedShapes
+        {
+            public PhysicsShape Shape;
+            public int OwnerKey;
+        }
+        
+        private NativeList<OwnedShapes> m_OwnedShapes;
+        
+        private void Reset()
+        {
+            if (SceneBody == null)
+                SceneBody = SceneBody.FindSceneBody(gameObject);
+        }
+       
+        private void OnEnable()
+        {
+            Reset();
+        
+            if (SceneBody != null)
+            {
+                SceneBody.CreateBodyEvent += OnCreateBody;
+                SceneBody.DestroyBodyEvent += OnDestroyBody;
+            }
+            
+            m_OwnedShapes = new NativeList<OwnedShapes>(Allocator.Persistent);
+            
+            CreateShapes();
+        }
+
+        private void OnDisable()
+        {
+            DestroyShapes();
+
+            if (m_OwnedShapes.IsCreated)
+                m_OwnedShapes.Dispose();
+            
+            if (SceneBody != null)
+            {
+                SceneBody.CreateBodyEvent -= OnCreateBody;
+                SceneBody.DestroyBodyEvent -= OnDestroyBody;
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (!isActiveAndEnabled)
+                return;
+
+            DestroyShapes();
+            CreateShapes();
+        }
+
+        private void Update()
+        {
+            if (Application.isPlaying ||
+                !transform.hasChanged)
+                return;
+            
+            // Create the shapes.
+            CreateShapes();
+        }
+        
+        private void CreateShapes()
+        {
+            // Destroy any existing shape.
+            DestroyShapes();
+
+            if (!m_OwnedShapes.IsCreated)
+                return;
+            
+            if (!SceneBody)
+                return;
+
+            var body = SceneBody.Body;
+            if (!body.isValid)
+                return;
+
+            if (Sprite == null)
+                return;
+
+            var physicsShapeCount = Sprite.GetPhysicsShapeCount();
+            if (physicsShapeCount == 0)
+                return;
+
+            var composer = PhysicsComposer.Create();
+            var vertexPath = new NativeList<Vector2>(Allocator.Temp);
+            
+            // Add all physic shape paths.
+            for (var i = 0; i < physicsShapeCount; ++i)
+            {
+                // Get the physics shape.
+                if (Sprite.GetPhysicsShape(i, m_PhysicsShapeVertex) > 0)
+                {
+                    // Add to something we can use.
+                    foreach (var vertex in m_PhysicsShapeVertex)
+                        vertexPath.Add(vertex);
+                    
+                    // Add the layer to the composer.
+                    composer.AddLayer(vertexPath.AsArray(), PhysicsTransform.identity);
+                }
+            }
+            
+            // Calculate the polygons from the points.
+            using var polygons = composer.CreatePolygonGeometry();
+
+            vertexPath.Dispose();
+            composer.Destroy();
+            
+            // Calculate the relative transform from the scene body to this scene shape.
+            var relativeTransform = PhysicsMath.GetRelativeMatrix(SceneBody.transform, transform, SceneBody.Body.world.transformPlane);
+
+            // Iterate the polygons.
+            foreach (var geometry in polygons)
+            {
+                if (!geometry.isValid)
+                    continue;
+                
+                var shapeGeometry = geometry.Transform(relativeTransform, false);
+                if (!shapeGeometry.isValid)
+                    continue;
+                
+                var shape = body.CreateShape(shapeGeometry, ShapeDefinition);
+                if (!shape.isValid)
+                    continue;
+
+                // Set the user data.
+                shape.userData = UserData;
+                
+                // Set the callback target.
+                shape.callbackTarget = CallbackTarget;
+                
+                // Set the owner.
+                var ownerKey = shape.SetOwner(this);
+                
+                // Add to owned shapes.
+                m_OwnedShapes.Add(new OwnedShapes
+                {
+                    Shape = shape,
+                    OwnerKey = ownerKey
+                });
+            }
+        }
+        
+        private void DestroyShapes()
+        {
+            if (!m_OwnedShapes.IsCreated)
+                return;
+            
+            foreach (var ownedShape in m_OwnedShapes)
+            {
+                if (ownedShape.Shape.isValid)
+                    ownedShape.Shape.Destroy(updateBodyMass: false, ownerKey: ownedShape.OwnerKey);
+            }
+
+            m_OwnedShapes.Clear();
+
+            if (SceneBody.Body.isValid)
+                SceneBody.Body.ApplyMassFromShapes();
+        }
+        
+        private void OnCreateBody(SceneBody sceneBody)
+        {
+            CreateShapes();
+        }
+
+        private void OnDestroyBody(SceneBody sceneBody)
+        {
+            DestroyShapes();
+        }        
+    }
+}
