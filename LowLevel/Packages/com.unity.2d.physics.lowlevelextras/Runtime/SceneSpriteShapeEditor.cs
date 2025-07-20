d3b3d090465e52646dacfc72a6601407a3cc4710
Melvyn May <melv@unity3d.com>
2025-07-20 14:33:55 +0100

    SceneSpriteShape (WIP)

---
+++ LowLevel/Packages/com.unity.2d.physics.lowlevelextras/Runtime/SceneSpriteShapeEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor.UIElements;
+using UnityEngine.U2D.Physics.LowLevelExtras;
+using UnityEngine.UIElements;
+
+namespace UnityEditor.U2D.Physics.LowLevelExtras
+{
+    [CustomEditor(typeof(SceneSpriteShape))]
+    [CanEditMultipleObjects]
+    public class SceneSpriteShapeEditor : Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = new VisualElement();
+
+            root.Add(new PropertyField(serializedObject.FindProperty(nameof(SceneSpriteShape.Sprite))));
+            root.Add(new PropertyField(serializedObject.FindProperty(nameof(SceneSpriteShape.ShapeDefinition))));
+            root.Add(new PropertyField(serializedObject.FindProperty(nameof(SceneSpriteShape.UserData))));
+            root.Add(new PropertyField(serializedObject.FindProperty(nameof(SceneSpriteShape.CallbackTarget))));
+            root.Add(new PropertyField(serializedObject.FindProperty(nameof(SceneSpriteShape.SceneBody))));
+
+            return root;
+        }
+    }
+}
