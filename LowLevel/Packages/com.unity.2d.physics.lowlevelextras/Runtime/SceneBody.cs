32aab47aa383c5d8c68fc9210c2c9fa2480eff3d
Melvyn May <melv@unity3d.com>
2025-07-25 17:10:19 +0100

    Only update the PhysicsBody.transformObject to the Transform if the body TransformWriteMode isn't Off.

--- LowLevel/Packages/com.unity.2d.physics.lowlevelextras/Runtime/SceneBody.cs
+++ LowLevel/Packages/com.unity.2d.physics.lowlevelextras/Runtime/SceneBody.cs
@@ -136,7 +136,7 @@ namespace UnityEngine.U2D.Physics.LowLevelExtras
             if (m_Body.isValid)
             {
                 // Set the transform object.
-                m_Body.transformObject = transform;
+                m_Body.transformObject = m_Body.transformWriteMode != PhysicsBody.TransformWriteMode.Off ? transform : null;
 
                 // Set the user data.
                 m_Body.userData = UserData;
