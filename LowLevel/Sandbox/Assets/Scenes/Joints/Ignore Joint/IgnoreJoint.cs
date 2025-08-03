928a3db1d77c152d71f87bb2c02164799507754b
Melvyn May <melv@unity3d.com>
2025-08-03 14:00:53 +0100

    Add "IgnoreJoint" scene.

---
+++ LowLevel/Sandbox/Assets/Scenes/Joints/Ignore Joint/IgnoreJoint.cs
@@ -0,0 +1,139 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.LowLevelPhysics2D;
+using UnityEngine.UIElements;
+
+public class IgnoreJoint : MonoBehaviour
+{
+    private SandboxManager m_SandboxManager;	
+    private SceneManifest m_SceneManifest;
+    private UIDocument m_UIDocument;
+    private CameraManipulator m_CameraManipulator;
+
+    private PhysicsJoint m_Joint;
+    private PhysicsBody m_BodyA;
+    private PhysicsBody m_BodyB;
+
+    private bool m_EnableJoint;
+    
+    private void OnEnable()
+    {
+        m_SandboxManager = FindFirstObjectByType<SandboxManager>();
+        m_SceneManifest = FindFirstObjectByType<SceneManifest>();
+        m_UIDocument = GetComponent<UIDocument>();
+
+        m_CameraManipulator = FindFirstObjectByType<CameraManipulator>();
+        m_CameraManipulator.CameraSize = 10f;
+        m_CameraManipulator.CameraStartPosition = new Vector2(0f, 7f);
+
+        // Set up the scene reset action.
+        m_SandboxManager.SceneResetAction = SetupScene;
+
+        // Set Overrides.
+        m_SandboxManager.SetOverrideDrawOptions(overridenOptions: PhysicsWorld.DrawOptions.AllJoints, fixedOptions: PhysicsWorld.DrawOptions.AllJoints);
+
+        m_EnableJoint = true;
+        
+        SetupOptions();
+
+        SetupScene();
+    }
+
+    private void OnDisable()
+    {
+        // Reset overrides.
+        m_SandboxManager.ResetOverrideDrawOptions();
+    }
+
+    private void SetupOptions()
+    {
+        var root = m_UIDocument.rootVisualElement;
+        
+        {
+            // Menu Region (for camera manipulator).
+            var menuRegion = root.Q<VisualElement>("menu-region");
+            menuRegion.RegisterCallback<PointerEnterEvent>(_ => ++m_CameraManipulator.OverlapUI );
+            menuRegion.RegisterCallback<PointerLeaveEvent>(_ => --m_CameraManipulator.OverlapUI );
+            
+            // Enable Joint.
+            var enableJoint = root.Q<Toggle>("enable-joint");
+            enableJoint.value = m_EnableJoint;
+            enableJoint.RegisterValueChangedCallback(evt =>
+            {
+                m_EnableJoint = evt.newValue;
+                UpdateJoint();
+            });              
+            
+            // Reset Scene.
+            var resetScene = root.Q<Button>("reset-scene");
+            resetScene.clicked += SetupScene;
+            
+            // Fetch the scene description.
+            var sceneDescription = root.Q<Label>("scene-description");
+            sceneDescription.text = $"\"{m_SceneManifest.LoadedSceneName}\"\n{m_SceneManifest.LoadedSceneDescription}";
+        }
+    }
+
+    private void SetupScene()
+    {
+        // Reset the scene state.
+        m_SandboxManager.ResetSceneState();
+
+        var world = PhysicsWorld.defaultWorld;
+        var bodies = m_SandboxManager.Bodies;
+
+        // Ground Body.
+        {
+            var groundBody = world.CreateBody(PhysicsBodyDefinition.defaultDefinition);
+            bodies.Add(groundBody);
+            
+            var vertices = new NativeList<Vector2>(Allocator.Temp);
+            vertices.Add(Vector2.right * 17f + Vector2.up * 17f);
+            vertices.Add(Vector2.right * 17f);
+            vertices.Add(Vector2.left * 17f);
+            vertices.Add(Vector2.left * 17f + Vector2.up * 17f);
+
+            var geometry = new ChainGeometry(vertices.AsArray());
+            groundBody.CreateChain(geometry, PhysicsChainDefinition.defaultDefinition);
+        }
+
+        // Obstacle Body.
+        {
+            var geometry = PolygonGeometry.CreateBox(size: new Vector2(2f, 6f));
+            
+            var body = world.CreateBody(new PhysicsBodyDefinition { bodyType = RigidbodyType2D.Dynamic, position = new Vector2(0f, 3f)});
+            bodies.Add(body);
+            body.CreateShape(geometry);
+        }
+        
+        // Ignored Bodies.
+        {
+            var geometry = PolygonGeometry.CreateBox(size: new Vector2(4f, 4f));
+            
+            m_BodyA = world.CreateBody(new PhysicsBodyDefinition { bodyType = RigidbodyType2D.Dynamic, position = new Vector2(-4f, 2f)});
+            bodies.Add(m_BodyA);
+            m_BodyA.CreateShape(geometry);
+
+            m_BodyB = world.CreateBody(new PhysicsBodyDefinition { bodyType = RigidbodyType2D.Dynamic, position = new Vector2(4f, 2f)});
+            bodies.Add(m_BodyB);
+            m_BodyB.CreateShape(geometry);
+
+            UpdateJoint();
+        }
+    }
+
+    private void UpdateJoint()
+    {
+        // Destroy the joint if it's valid.
+        if (m_Joint.isValid)
+            m_Joint.Destroy();
+
+        // Finish if the joint is not enabled.
+        if (!m_EnableJoint)
+            return;
+        
+        // Create the joint.
+        var world = PhysicsWorld.defaultWorld;
+        m_Joint = world.CreateJoint(new PhysicsIgnoreJointDefinition { bodyA = m_BodyA, bodyB = m_BodyB });
+    }
+}
