245902c4a8a475f65d51ab55fa6e198688124fb5
Melvyn May <melv@unity3d.com>
2025-10-07 12:06:32 +0100

    [LowLevel] Ensure the Sandbox scene is loaded when entering play mode.

---
+++ LowLevel/Projects/Sandbox/Assets/Editor/StartupScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class StartupScene
+{
+    static StartupScene()
+    {
+        const string sceneStartUpName = "Assets/Sandbox.unity";
+        EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(sceneStartUpName);
+    }
+}
