81fc2f06ab958c7922e2adf155dbce41afff219a
Melvyn May <melv@unity3d.com>
2025-11-24 19:08:21 +0000

    [Snippet] Added "SuggestedAPI" snippet.

---
+++ LowLevel/Projects/Snippets/Assets/Examples/SuggestedAPI/PhysicsAPIExtensions.cs
@@ -0,0 +1,94 @@
+using System;
+using UnityEngine.LowLevelPhysics2D;
+
+public static class PhysicsAPIExtensions
+{
+    #region Shape-Distance
+    
+    /// <summary>
+    /// Get the minimum distance between shapes.
+    /// </summary>
+    /// <param name="physicsShape"></param>
+    /// <param name="otherShape"></param>
+    /// <param name="useRadii"></param>
+    /// <returns></returns>
+    public static PhysicsQuery.DistanceResult ShapeDistance(this PhysicsShape physicsShape, PhysicsShape otherShape, bool useRadii = true)
+    {
+        return PhysicsQuery.ShapeDistance(
+            new PhysicsQuery.DistanceInput
+            {
+                shapeProxyA = physicsShape.CreateShapeProxy(),
+                shapeProxyB = otherShape.CreateShapeProxy(),
+                transformA = physicsShape.body.transform,
+                transformB = otherShape.body.transform,
+                useRadii = useRadii
+            });
+    }
+
+    /// <summary>
+    /// Get the minimum distance between a shape and a span of shapes.
+    /// </summary>
+    /// <param name="physicsShape"></param>
+    /// <param name="otherShapes"></param>
+    /// <param name="useRadii"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static PhysicsQuery.DistanceResult ShapeDistance(this PhysicsShape physicsShape, ReadOnlySpan<PhysicsShape> otherShapes, bool useRadii = true)
+    {
+        if (!physicsShape.isValid)
+            throw new ArgumentException(nameof(physicsShape));
+        
+        var bestDistance = float.MaxValue;
+        PhysicsQuery.DistanceResult bestResult = default;
+
+        // Fetch shape A details.
+        var shapeProxyA = physicsShape.CreateShapeProxy();
+        var transformA = physicsShape.body.transform;
+        
+        // Iterate all the shapes.
+        foreach (var otherShape in otherShapes)
+        {
+            if (!otherShape.isValid)
+                throw new ArgumentException(nameof(physicsShape));
+            
+            // Query the distance.
+            var result = PhysicsQuery.ShapeDistance(
+                new PhysicsQuery.DistanceInput
+                {
+                    shapeProxyA = shapeProxyA,
+                    transformA = transformA,
+                    shapeProxyB = otherShape.CreateShapeProxy(),
+                    transformB = otherShape.body.transform,
+                    useRadii = useRadii
+                });
+
+            // Ignore if further away.
+            if (result.distance >= bestDistance)
+                continue;
+            
+            // Found a better distance.
+            bestDistance = result.distance;
+            bestResult = result;
+        }
+
+        return bestResult;
+    }
+
+    /// <summary>
+    /// Get the minimum distance between a shape and all shapes attached to the specified body.
+    /// </summary>
+    /// <param name="physicsShape"></param>
+    /// <param name="physicsBody"></param>
+    /// <param name="useRadii"></param>
+    /// <returns></returns>
+    public static PhysicsQuery.DistanceResult ShapeDistance(this PhysicsShape physicsShape, PhysicsBody physicsBody, bool useRadii = true)
+    {
+        // Get all the body shapes.
+        using var otherShapes = physicsBody.GetShapes();
+
+        // Calculate the minimum distance.
+        return physicsShape.ShapeDistance(otherShapes, useRadii);
+    }
+
+    #endregion
+}
---
+++ LowLevel/Projects/Snippets/Assets/Examples/SuggestedAPI/README.md
@@ -0,0 +1,8 @@
+# Miscellaneous Suggestions for API
+
+This snippet contains implementations for suggestions to the API.
+When or if the API is implemented, these suggestions will be removed and noted here.
+
+## Suggestions
+- [Thread](https://discussions.unity.com/t/lowlevel-physics-question/1696693/2) - PhysicsQuery.ShapeDistance extended to include a span of shapes and other utility.
+  - See `PhysicsAPIExtensions.Shape-Distance`
