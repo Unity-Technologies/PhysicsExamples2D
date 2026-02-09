d66107b71de1b1763c30bcdb919f0d7488a4d139
Melvyn May <melv@unity3d.com>
2026-02-09 08:36:19 +0000

    Fix Icon Path for components.

--- PhysicsCore2D/Packages/com.unity.2d.physics.extras/Runtime/Utility/IconUtility.cs
+++ PhysicsCore2D/Packages/com.unity.2d.physics.extras/Runtime/Utility/IconUtility.cs
@@ -5,6 +5,6 @@ namespace Unity.U2D.Physics.Extras
     [MovedFrom(autoUpdateAPI: APIUpdates.AutoUpdateAPI, sourceNamespace: APIUpdates.RuntimeSourceNamespace)]
     public static class IconUtility
     {
-        public const string IconPath = "Packages/com.unity.2d.physics.lowlevelextras/Editor/Icons/";
+        public const string IconPath = "Packages/com.unity.2d.physics.extras/Editor/Icons/";
     }
 }
